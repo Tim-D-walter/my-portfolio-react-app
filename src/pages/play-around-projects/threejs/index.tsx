ec7d2c20a5e8f66e3b3b80770d7dad88edff1e0c
Tim Walter <tim.walter@lhind.dlh.de>
2023-04-24 22:45:52 +0200

    First threejs test with a canvas, a floor and orbitcontrols

--- src/pages/play-around-projects/threejs/index.tsx
+++ src/pages/play-around-projects/threejs/index.tsx
@@ -1,6 +1,13 @@
 import {motion} from "framer-motion";
 import Image from "next/image";
 import {Inter} from "next/font/google";
+import { createRoot } from 'react-dom/client'
+import React, { useRef, useState } from 'react'
+import { Canvas, useFrame } from '@react-three/fiber'
+import Floor from "./Floor";
+import OrbitControls from "./OrbitControls"
+import Script from 'next/script'
+
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,25 +25,16 @@ export default function Home() {
         >
             <main className={`wrapper-container ${inter.className}`}>
                 <div className="about-container">
-                    <div
-                        className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px]
-            before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent
-            before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px]
-            after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl
-            after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10
-            after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]"
+                    <Canvas
+                        shadows
+                        camera={{
+                            position: [-6, 7, 7],
+                        }}
                     >
-                        <Image
-                            className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] rounded-full"
-                            src="/NoPic.png"
-                            alt="This will show a threeJs project when I am finished"
-                            width={300}
-                            height={200}
-                            priority
-                        />
-                    </div>
-                    <p>**This will show a threeJs project when I am finished**</p>
-
+                        <OrbitControls />
+                        <ambientLight color={"white"} intensity={0.3} />
+                        <Floor/>
+                    </Canvas>
                 </div>
             </main>
         </motion.div>
